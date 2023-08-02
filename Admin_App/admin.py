3752258b2ea45637763e28b70f2ee2fcde4a504e
muhsinmuhsy <muhsinkoodali856@gmail.com>
2023-08-02 12:03:35 +0530

    first commit

--- Admin_App/admin.py
+++ Admin_App/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from Admin_App.models import *
 # Register your models here.
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
--- Admin_App/models.py
+++ Admin_App/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='product_image', null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
